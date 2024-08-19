50bfd27dbfe0363c6da89fd7ba64f9e895316f22
Aashish Kumar <ritikkamboj4314@gmail.com>
2024-08-19 13:50:44 +0530

    applied the click functionality on meal img which dynamically add the name and ingredients in UI

--- script.js
+++ script.js
@@ -6,6 +6,52 @@ const resultHeading = document.getElementById("result-heading");
 
 const singleMeal = document.getElementById("single-meal");
 
+function getMealById(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      console.log(meal)
+
+      addMealToDOM(meal);
+    });
+}
+
+
+function addMealToDOM(meal)
+{
+  const ingredients =[];
+
+ 
+
+  for(let i =1 ; i<=20; i++)
+  {
+    if(meal[`strIngredient${i}`])
+    {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+      console.log(ingredients);
+    }
+    else{
+      break; 
+    }
+  }
+  singleMeal.innerHTML= `<div class ="single-meal">
+  <h1>${meal.strMeal}</h1>
+  <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+  <div class = 'single-meal-info'>
+  ${meal.strcategory ? `<p>${meal.strCategory}</p>` : ``}
+  ${meal.strCategory ? `<p>${meal.strArea }</p>` : ``}
+  </div>
+  <div class = 'main'>
+    <p>${meal.strInstructions}</p>
+    <h2>Ingredients</h2>
+    <ul>
+    ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+    </ul>
+  </div>
+  </div>`
+}
+
 function searchMeal(e) {
   e.preventDefault();
   singleMeal.innerHTML = "";
@@ -40,6 +86,22 @@ function searchMeal(e) {
 }
 
 submit.addEventListener("submit", searchMeal);
+
+mealEl.addEventListener("click", (e) => {
+  const mealInfo = e.composedPath().find((item) => {
+    if (item.classList) {
+      return item.classList.contains("meal-info");
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealId = mealInfo.getAttribute("data-mealID");
+    console.log(mealId);
+    getMealById(mealId);
+  }
+});
 
 // `<div class='meal'>
 // <img src="${meal.strMealThumb}" alt="${meal.strMealThumb}"/>
